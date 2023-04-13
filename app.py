2114816d9bf68626c4221d5415a65f53119115a3
vineet Jain <vineet.jain@ukg.com>
2023-04-13 15:27:33 +0530

    enhancement

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import openai
 import re
+import fitz
 import tiktoken as tk
 import json
 import os
 import numpy as np
 import pandas as pd
+import time
+from datetime import datetime
 
 app = Flask(__name__)
 CORS (app) 
@@ -38,9 +41,10 @@ def num_tokens_from_string (string, encoding_name):
     return num_tokens
 
 def train_data (text):
-    openai.api_key ="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"
+    openai.api_key ="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"
     token = num_tokens_from_string (text, 'cl100k_base')
     embedding = get_embedding (text)
+    ts = datetime.now ()
     data = {
         'summary': {text},
         'token': {token}
@@ -52,7 +56,7 @@ def train_data (text):
     df1['embedding']= df1['summary'].apply(get_embedding)
 
 def prompt_lookup(text):
-    openai.api_key ="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"
+    openai.api_key ="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"
     output_path='my_csv.csv'
     df = pd.read_csv(output_path)
     df['embedding']= df['summary'].apply(get_embedding)
@@ -70,15 +74,15 @@ def vector_similarity (vec1, vec2):
     return np.dot (np.array(vec1),np.array(vec2))
 
 # Example endpoint for POST requests
-@app.route('/getSummary', methods=['POST'])
+@app.route('/assist', methods=['POST'])
 def getSummery():
     data = request.get_json()
     #print(data)
     input_text = data['input']
-    prompt = prompt_lookup(input_text)
     if (('hi' == input_text.lower()) or ('hello' == input_text.lower())):
         return jsonify({'result':'How can I assist you today, I am here to generate summary for your given input..'})
-    openai.api_key = "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"
+    openai.api_key = "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"
+    prompt = prompt_lookup(input_text)
     response = openai.Completion.create(
     engine="text-davinci-003",
     prompt=prompt,
@@ -89,47 +93,57 @@ def getSummery():
     )
     #print(response)
     summary = response.choices[0].text.strip()
-    
-    #summary = response
-    #print(summary)
-    #response = openai.ChatCompletion.create(
-    #model='gpt-3.5-turbo',
-    #api_key = "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",
-    #messages=[{'role':'system','content':'You are good at creating bullet point summaries'},
-    #{'role':'user','content':f"Summarize the following in bullet point form:\n{summary}"}
-    #]
-    #)
-    #print(response)
-    #return response.choices[0].message.content
-    #summary = re.sub(r'\n', ' ', summary)
-    #summary = re.sub(r'\s+', ' ', summary)
-    #print(summary)
-    #return jsonify({'result': response.choices[0].message.content})
     return jsonify({'result': summary})
-    #return response.choices[0].message.content
 
 # Example for transcript
-@app.route('/getScript', methods=['GET'])
+@app.route('/uploadAndTrainData', methods=['POST'])
 def getScript():
     model_id = 'whisper-1'
-    file_path = "C:/Users/chirag/Downloads/AWS.mp3"
-    #file_path = "C:/Users/chirag/Downloads/WAudio.mpeg"
-    #You are good at creating bullet point summaries and have knowledge of upcoming vacation plan
-    audio_file = open(file_path, "rb")
-    response = openai.Audio.transcribe(
-    api_key = "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",
-    model = model_id,
-    file= audio_file
-    )
-    summary = response
+    date = request.form.get('date');
+    agenda = request.form.get('agenda');
+    organiser = request.form.get('Organiser');
+    ts = datetime.now()
+    
+    print("Posted file: {}".format(request.files['files']))
+    audio_file = request.files['files']
+    currentWorkingDirectory = [os.getcwd(), "\\uploads\\", ts.strftime("%m%d%Y_%H%M%S"), "_" , audio_file.filename ]
+    file_path = "".join(currentWorkingDirectory)
+    audio_file.save(file_path)
+
+    
+    file_ext = os.path.splitext(audio_file.filename)[1]
+
+    if file_ext == ".pdf":
+        with fitz.open(file_path) as doc:
+            text = ""
+            for page in doc:
+                text += page.get_text()
+            summary = text
+            print(summary)
+        response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        api_key = "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",
+        messages=[{'role':'system','content':'You are good at creating bullet point summaries and have knowledge of AWS '},
+        {'role':'user','content':f"Summarize the following in bullet point form:\n{summary}"}
+        ]
+        )
+    else:
+        audio_file1 = open(file_path, "rb")
+        response = openai.Audio.transcribe(
+        api_key = "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",
+        model = model_id,
+        file= audio_file1)
+        summary = response
+        print(summary)
+        response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        api_key = "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",
+        messages=[{'role':'system','content':'You are good at creating bullet point summaries and have knowledge of AWS '},
+        {'role':'user','content':f"Summarize the following in bullet point form:\n{summary['text']}"}
+        ]
+        )
     #print(summary)
-    response = openai.ChatCompletion.create(
-    model='gpt-3.5-turbo',
-    api_key = "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",
-    messages=[{'role':'system','content':'You are good at creating bullet point summaries and have knowledge of AWS '},
-    {'role':'user','content':f"Summarize the following in bullet point form:\n{summary['text']}"}
-    ]
-    )
+    train_data(f"Agenda of this meeting is {agenda}. Organised or authour is/are:  {organiser}. this meeting held on: {date} content: {response.choices[0].message.content}")
     return response.choices[0].message.content
 
 # Example for transcript
@@ -150,12 +164,12 @@ def getScriptSummary():
     #print(summary)
     response = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
-    api_key = "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",
+    api_key = "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",
     messages=[{'role':'system','content':'You are good at creating bullet point summaries and have knowledge of AWS '},
     {'role':'user','content':f"Summarize the following in bullet point form:\n{summary['text']}"}
     ]
     )
-    train_data(f"purpose: aws discussion organisation: sample unicorn, content:{response.choices[0].message.content}, date:20-12-2022")
+    #train_data(f"purpose: aws discussion organisation: sample unicorn, content:{response.choices[0].message.content}, date:20-12-2022")
     return response.choices[0].message.content
     
 
